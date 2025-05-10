74794fb1d60f196ddb28a26ec332805e5b1c7211
pi_thon <pithon3.142@gmail.com>
2025-05-10 21:22:58 +0200

    removed column team_id from users tables

--- Backend/sql/hockey_db.sql
+++ Backend/sql/hockey_db.sql
@@ -34,6 +34,7 @@ create table team (
     league team_league not null,
     logo_url text
 );
+
 -- events table
 create table events (
     event_id serial primary key,
@@ -54,18 +55,16 @@ create table users (
     password text not null,
     user_role user_role not null,
     date_created timestamp default current_timestamp,
-    team_id int not null references team(team_id)
 );
 
 --  users' sub-type table for users referencing team_id [coach, players]
 create table team_manager (
     user_id int primary key references users(user_id),
-    team_id int not null  references team(team_id)
+    team_id int not null references team(team_id)
 );
 
 alter table users
 add column email varchar(200) not null unique;
-
 
 -- invitations joint table : many to many relationship between users and events
 create table user_events_invitations (
@@ -73,8 +72,6 @@ create table user_events_invitations (
     events_id int not null references events(event_id),
     user_id int not null references users(user_id)
 );
-
-
 
 -- announcements table
 create table announcements (
