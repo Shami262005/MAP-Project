9586f9ad5b6da1d35251b7c0f25b73788c11069f
pi_thon <pithon3.142@gmail.com>
2025-05-11 18:34:04 +0200

    database session

---
+++ Backend/Hockey-API/hockey_db.session.sql
@@ -0,0 +1,6 @@
+insert into users (first_name, last_name, phone, username, password, user_role, team_id, email)
+values ('Lina', 'Canny', '+62 96 5748 458', 'girly', 'mahenry', 'player', 1, 'lana@gmail.com')
+
+
+insert into team (team_name, contact, team_address, category, league, logo_url)
+values ('Hockey Hawks', '061 385 4785', '2345, Strange street, Wanaheda, Windhoek', 'female', 'first division', 'url/url/url')
